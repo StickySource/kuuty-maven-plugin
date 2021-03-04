c68e5493c7f0b2a9c6292ac331253b0c766eb5a9
Michael McCallum <michael@stickycode.net>
2021-03-04 23:01:48 +1300

    Add some plugin tests to prove its working correctly:
    interestingly the namespace is interpolated, not sure what the means for putting placeholders into it

--- src/test/java/net/stickycode/plugin/kuuty/KuutyResourceGeneratorTest.java
+++ src/test/java/net/stickycode/plugin/kuuty/KuutyResourceGeneratorTest.java
@@ -17,55 +17,107 @@ public class KuutyResourceGeneratorTest {
 
   @Test
   public void writeConfig() throws IOException {
-    KuutyConfigMapProcessor other = new KuutyConfigMapProcessor("theconfig", "thenamespace");
+    KuutyConfigMapProcessor other = new KuutyConfigMapProcessor("theconfig", null);
     other.processTemplate("one.properties", "a=value");
     other.processTemplate("some.xml", "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-        "<configuration>\n" +
-        "  <root level=\"info\" />\n" +
-        "</configuration>\n");
+      "<configuration>\n" +
+      "  <root level=\"info\" />\n" +
+      "</configuration>\n");
 
     Path target = Path.of("target");
     Files.createDirectories(target);
     new KuutyResourceGenerator().write(other.getResource(), target, "config.yaml");
     assertThat(target.resolve("config.yaml")).hasContent("apiVersion: v1\n" +
-        "data:\n" +
-        "  one.properties: a=value\n" +
-        "  some.xml: |\n" +
-        "    <?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-        "    <configuration>\n" +
-        "      <root level=\"info\" />\n" +
-        "    </configuration>\n" +
-        "kind: ConfigMap\n" +
-        "metadata:\n" +
-        "  name: theconfig\n" +
-        "  namespace: thenamespace");
+      "data:\n" +
+      "  one.properties: a=value\n" +
+      "  some.xml: |\n" +
+      "    <?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+      "    <configuration>\n" +
+      "      <root level=\"info\" />\n" +
+      "    </configuration>\n" +
+      "kind: ConfigMap\n" +
+      "metadata:\n" +
+      "  name: theconfig");
+  }
+
+  @Test
+  public void writeConfigWithNamespace() throws IOException {
+    KuutyConfigMapProcessor other = new KuutyConfigMapProcessor("theconfig", "thenamespace");
+    other.processTemplate("one.properties", "a=value");
+    other.processTemplate("some.xml", "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+      "<configuration>\n" +
+      "  <root level=\"info\" />\n" +
+      "</configuration>\n");
+
+    Path target = Path.of("target");
+    Files.createDirectories(target);
+    new KuutyResourceGenerator().write(other.getResource(), target, "config.yaml");
+    assertThat(target.resolve("config.yaml")).hasContent("apiVersion: v1\n" +
+      "data:\n" +
+      "  one.properties: a=value\n" +
+      "  some.xml: |\n" +
+      "    <?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+      "    <configuration>\n" +
+      "      <root level=\"info\" />\n" +
+      "    </configuration>\n" +
+      "kind: ConfigMap\n" +
+      "metadata:\n" +
+      "  name: theconfig\n" +
+      "  namespace: thenamespace");
   }
 
   @Test
   public void writeSecret() throws IOException {
-    KuutySecretProcessor other = new KuutySecretProcessor("thesecret", "thenamespace");
+    KuutySecretProcessor other = new KuutySecretProcessor("thesecret", null);
     other.processTemplate("one.properties", "a=value");
     other.processTemplate("some.xml", "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-        "<configuration>\n" +
-        "  <root level=\"info\" />\n" +
-        "</configuration>\n");
+      "<configuration>\n" +
+      "  <root level=\"info\" />\n" +
+      "</configuration>\n");
 
     Path target = Path.of("target");
     Files.createDirectories(target);
     new KuutyResourceGenerator().write(other.getResource(), target, "secret.yaml");
     assertThat(target.resolve("secret.yaml")).hasContent("apiVersion: v1\n" +
-        "kind: Secret\n" +
-        "metadata:\n" +
-        "  name: thesecret\n" +
-        "  namespace: thenamespace\n" +
-        "stringData:\n" +
-        "  one.properties: a=value\n" +
-        "  some.xml: |\n" +
-        "    <?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-        "    <configuration>\n" +
-        "      <root level=\"info\" />\n" +
-        "    </configuration>\n" +
-        "type: Opaque\n");
+      "kind: Secret\n" +
+      "metadata:\n" +
+      "  name: thesecret\n" +
+      "stringData:\n" +
+      "  one.properties: a=value\n" +
+      "  some.xml: |\n" +
+      "    <?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+      "    <configuration>\n" +
+      "      <root level=\"info\" />\n" +
+      "    </configuration>\n" +
+      "type: Opaque");
+
+  }
+
+  @Test
+  public void writeSecretWithNamespace() throws IOException {
+    KuutySecretProcessor other = new KuutySecretProcessor("thesecret", "thenamespace");
+    other.processTemplate("one.properties", "a=value");
+    other.processTemplate("some.xml", "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+      "<configuration>\n" +
+      "  <root level=\"info\" />\n" +
+      "</configuration>\n");
+
+    Path target = Path.of("target");
+    Files.createDirectories(target);
+    new KuutyResourceGenerator().write(other.getResource(), target, "secret.yaml");
+    assertThat(target.resolve("secret.yaml")).hasContent("apiVersion: v1\n" +
+      "kind: Secret\n" +
+      "metadata:\n" +
+      "  name: thesecret\n" +
+      "  namespace: thenamespace\n" +
+      "stringData:\n" +
+      "  one.properties: a=value\n" +
+      "  some.xml: |\n" +
+      "    <?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+      "    <configuration>\n" +
+      "      <root level=\"info\" />\n" +
+      "    </configuration>\n" +
+      "type: Opaque\n");
 
   }
 
